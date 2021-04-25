a13ec5621e411e6832bb189673e52485f53ef764
Tori Sanford <tori.danielle.clark91@gmail.com>
2021-04-24 21:44:38 -0400

    found error and corrected file location on db for prompts

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
 // create the DB
 // db will be mysql
-const mysql = require("mysql")
+const mysql = require("mysql");
 // where it will get the prompts from
-const prompts = require("./library.prompt.js")
-
-
+const prompts = require("./library/prompt");
 // make connection
 const connection = mysql.createConnection({
     host: "localhost",
@@ -23,4 +21,20 @@ connection.connect(function (err) {
 
 exports.connection = connection
 
-
+// Documentation from  https://www.npmjs.com/package/mysql
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'me',
+//   password : 'secret',
+//   database : 'my_db'
+// });
+ 
+// connection.connect();
+ 
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+ 
+// connection.end();
